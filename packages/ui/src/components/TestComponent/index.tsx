279457becb5efaa3c4de7bc767d8fdef24cee87f
alone-tyh <1305580673@qq.com>
2023-11-13 00:45:57 +0800

    first commit

---
+++ packages/ui/src/components/TestComponent/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const TestComponent: React.FC = () => {
+
+    return (
+        <div >
+            这是测试组件哈哈哈哈哈
+        </div>
+    )
+}
---
+++ packages/utils/src/index.ts
@@ -0,0 +1,10 @@
+
+/**
+ * 工具包测试方法
+ * @param testMesssage 
+ * @returns 
+ */
+export const testFn = (testMesssage: string) => {
+    console.log('工具包测试方法', testMesssage);
+    return Date.now();
+}
