34bb8db65c0cee6a8ebd5e6e33c850d34132aa98
alone-tyh <1305580673@qq.com>
2024-07-26 00:45:11 +0800

    svg渐变测试

--- apps/vite-react/src/App.tsx
+++ apps/vite-react/src/App.tsx
@@ -1,12 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { TestB, TestComponent } from '@monorepo-test/ui'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { TestB, TestComponent } from "@monorepo-test/ui";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
+  const width = 500,
+    height = 600,
+    radius = 60;
   return (
     <>
       <div>
@@ -29,10 +32,96 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div style={{ width, height, border: "1px solid black" }}>
+        <svg
+          width={width}
+          height={height}
+          style={{ width, height, borderRadius: 20 }}
+        >
+          <defs>
+            <linearGradient id="line-top" x1={0} y1={0} x2={0} y2={1}>
+              <stop offset={"0%"} stopColor="rgb(255, 0, 0)" stopOpacity={1} />
+              <stop
+                offset={"100%"}
+                stopColor="rgb(255, 255, 255)"
+                stopOpacity={0}
+              />
+            </linearGradient>
+            <linearGradient id="line-left" x1={0} y1={0} x2={1} y2={0}>
+              <stop offset={"0%"} stopColor="rgb(255, 0, 0)" stopOpacity={1} />
+              <stop
+                offset={"100%"}
+                stopColor="rgb(255, 255, 255)"
+                stopOpacity={0}
+              />
+            </linearGradient>
+            <linearGradient id="line-right" x1={1} y1={0} x2={0} y2={0}>
+              <stop offset={"0%"} stopColor="rgb(255, 0, 0)" stopOpacity={1} />
+              <stop
+                offset={"100%"}
+                stopColor="rgb(255, 255, 255)"
+                stopOpacity={0}
+              />
+            </linearGradient>
+            <linearGradient id="line-bottom" x1={0} y1={1} x2={0} y2={0}>
+              <stop offset={"0%"} stopColor="rgb(255, 0, 0)" stopOpacity={1} />
+              <stop
+                offset={"100%"}
+                stopColor="rgb(255, 255, 255)"
+                stopOpacity={0}
+              />
+            </linearGradient>
+            <radialGradient
+              id="radial-left-top"
+              cx={"50%"}
+              cy={"50%"}
+              r={"100%"}
+              fx="0%"
+              fy="0%"
+            >
+              <stop offset={"0%"} stopColor="rgb(255, 0, 0)" stopOpacity={1} />
+              <stop
+                offset={"100%"}
+                stopColor="rgb(255, 255, 255)"
+                stopOpacity={0}
+              />
+            </radialGradient>
+          </defs>
+          {/* <rect
+            x="0"
+            y="0"
+            width={"40"}
+            height={"40"}
+            fill={"url(#radial-left-top)"}
+          /> */}
+          <rect
+            x="0"
+            y="100% - 60"
+            width={"100%"}
+            height={radius}
+            fill={"url(#line-top)"}
+          />
+          <rect x="0" y="0" width={radius} height={"100%"} fill="url(#line-left)" />
+          <rect
+            x={width - radius}
+            y="0"
+            width={radius}
+            height={"100%"}
+            fill="url(#line-right)"
+          />
+          <rect
+            x="0"
+            y={height - radius}
+            width="100%"
+            height={radius}
+            fill="url(#line-bottom)"
+          />
+        </svg>
+      </div>
       <TestComponent />
-      <TestB  />
+      <TestB />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
